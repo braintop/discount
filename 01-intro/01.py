c79a4a7a0e2dccd3b52382086589ca842c7793ee
braintop <braintop7@gmail.com>
2025-12-16 09:42:29 +0200

    Add

---
+++ 01-intro/01.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+name = input(" What is your name? ")
+print(f"Hello, {name}")
+print("Hello,",name)
+
+# int , string , float , boolean
+num1 = int(input("enter num1 "))
+num2 = int(input("enter num2 "))
+solution = num1 + num2 # 1 int , "1" 
+print(solution)
